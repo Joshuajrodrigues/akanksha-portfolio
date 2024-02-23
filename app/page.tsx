591d5bdf713e6e4ab9a34957ea02434d842eb1d7
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-02-23 21:12:34 +0530

    refactored

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,13 @@
+import About from "@/components/about";
 import Navbar from "@/components/navbar";
-import Paragraph from "@/components/paragraph";
-import Image from "next/image";
-import img from "@/public/evening-sky.png";
+
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <main className="flex flex-col h-svh items-center justify-center  text-black">
-        <section className="w-full mt-8 p-8 items-center justify-center">
-          <div className="grid grid-cols-2 ">
-            <div className="grid grid-cols-2">
-              <Paragraph className="col-span-2 text-balance p-4">
-                im an experienced ui ux designer & art director passionate about
-                crafting creative digital experiences.
-              </Paragraph>
-              <div></div>
-              <Paragraph className="text-balance p-4">
-                im a ui ux designer & art director passionate about crafting
-                creative digital experiences.
-              </Paragraph>
-            </div>
-            <Image src={img} alt="evening sky" placeholder="blur" />
-          </div>
-        </section>
+      <About/>
       </main>
     </>
   );
---
+++ components/about.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Paragraph from './paragraph'
+import Image from 'next/image'
+import img from "@/public/evening-sky.png";
+const About = () => {
+  return (
+    <section className="w-full mt-8 p-8 items-center justify-center">
+          <div className="grid grid-cols-2 ">
+            <div className="grid grid-cols-2">
+              <Paragraph className="col-span-2 text-balance p-4">
+                im an experienced ui ux designer & art director passionate about
+                crafting creative digital experiences.
+              </Paragraph>
+              <div></div>
+              <Paragraph className="text-balance p-4">
+                im a ui ux designer & art director passionate about crafting
+                creative digital experiences.
+              </Paragraph>
+            </div>
+            <Image src={img} alt="evening sky" placeholder="blur" />
+          </div>
+        </section>
+  )
+}
+
+export default About
