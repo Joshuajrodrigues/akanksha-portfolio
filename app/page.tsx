e5c020f06dbb68fb0f8b7b4c95264dc03c882c19
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-03-09 23:34:10 +0530

    text anim fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Navmenu from "@/components/navmenu/navmenu";
-import Cursor from "@/components/typewriter/cursor";
 import TypeText from "@/components/typewriter/typeText";
 import Accent from "@/components/typography/accent";
 import Typography from "@/components/typography/typography";
@@ -7,8 +6,12 @@ import Typography from "@/components/typography/typography";
 export default function Home() {
   return (
     <main className=" flex flex-col h-svh items-center justify-center">
-      <Navmenu />
-      <TypeText/>
+      <Navmenu  />
+      <header className="text-start w-full p-2 m-1">
+
+      <TypeText hideCursor={true} text="Hello!"/>
+      <TypeText delay={1}/>
+      </header>
       <section>
       <Typography>
        i&apos;m a <Accent>uiux designer</Accent> passionate about crafting and <Accent>exploring experiences</Accent>  that stay with you.
--- components/typewriter/typeText.tsx
+++ components/typewriter/typeText.tsx
@@ -1,33 +1,43 @@
 "use client";
 import { animate, motion, useMotionValue, useTransform } from "framer-motion";
 import Cursor from "./cursor";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const TypeText = () => {
-    const baseText = "HELLO! I'M AKANKSHA";
-    const count = useMotionValue(0);
-    const rounded = useTransform(count, (latest) => Math.round(latest));
-    const displayText = useTransform(rounded, (latest) =>
-      baseText.slice(0, latest)
-    );
-    useEffect(() => {
-        const controls = animate(count, baseText.length, {
-          type: "tween", // Not really needed because adding a duration will force "tween"
-          duration: 1,
-          ease: "easeInOut",
-        });
-        return controls.stop;
-      }, []);
+const TypeText = ({
+  text = "I'M AKANKSHA.",
+  hideCursor = false,
+  delay=0
+}: {
+  text?: string;
+  hideCursor?: boolean;
+  delay?:number
+}) => {
+  const baseText = text;
+  const count = useMotionValue(0);
+  const [completed,setCompleted] = useState(false)
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const displayText = useTransform(rounded, (latest) =>
+    baseText.slice(0, latest),
+  );
+  useEffect(() => {
+    const controls = animate(count, baseText.length, {
+      type: "tween", // Not really needed because adding a duration will force "tween"
+      duration: 1,
+      ease: "easeInOut",
+      delay,
+      onComplete() {
+        setCompleted(true)
+      },
+    });
+    return controls.stop;
+  }, []);
 
-      
   return (
-   <span>
-    <motion.span>
-    {displayText}
-    </motion.span>
-    <Cursor/>
-   </span>
-  )
-}
+    <span>
+      <motion.h1 className="  text-lighTextPrimary font-black text-4xl">{displayText||" "}</motion.h1>
+     
+    </span>
+  );
+};
 
-export default TypeText
+export default TypeText;
