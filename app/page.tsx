a74e8491ddb0986c3645ca4ed5e607709f3a61b5
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-03-24 18:56:03 +0530

    scroll css test

--- app/page.tsx
+++ app/page.tsx
@@ -10,36 +10,32 @@ import Typography from "@/components/typography/typography";
 
 export default function Home() {
   return (
-    <main className=" m-8 flex  flex-col">
-      <div>
-        <Navmenu />
-      </div>
-      <header className="mt-8 flex h-24 w-full flex-col justify-center p-2 text-start">
-        <TypeText hideCursor={true} text="HELLO!" />
-        <TypeText delay={1} />
-      </header>
-      <section className="mt-8">
-        <Typography>
-          i&apos;m a <Accent>uiux designer</Accent> passionate about crafting
-          and <Accent>exploring experiences</Accent> that stay with you.
-        </Typography>
+    <main className=" relative  m-8 flex  flex-col">
+      <section className="panel">
+        <div>
+          <Navmenu />
+        </div>
+        <header className="mt-8 flex h-24 w-full flex-col justify-center p-2 text-start">
+          <TypeText hideCursor={true} text="HELLO!" />
+          <TypeText delay={1} />
+        </header>
+        <div className="mt-8">
+          <Typography>
+            i&apos;m a <Accent>uiux designer</Accent> passionate about crafting
+            and <Accent>exploring experiences</Accent> that stay with you.
+          </Typography>
+        </div>
       </section>
-      {/* <Typography>
-          This is a showcase of my professional and personal work. i hope you
-          enjoy it.
-        </Typography> */}
-      <section className="mt-8 min-h-svh ">
+      <section className="panel mt-8 min-h-svh ">
         <PageTitle title="Projects" />
         <div className="flex flex-col justify-center items-center space-y-4">
-
           <Card />
           <Card />
           <Card />
         </div>
       </section>
-      <section className="mt-8 min-h-svh flex flex-col justify-between items-center">
+      <section className="panel mt-8 min-h-svh flex flex-col justify-between items-center">
         <div>
-
           <PageTitle title="SAY Hello!" />
           <Typography>
             get in touch for any inquiries or just come say hi and we can discuss some cool things.
--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='h-8 mt-8'>All rights reserved.</div>
+        <div className='h-8 mt-8'>&copy; {new Date().getFullYear()} All rights reserved.</div>
     )
 }
 
